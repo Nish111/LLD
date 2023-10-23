f3365b0c3abae6205fcb25f9a6bfe72eba2976e3
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-10-23 08:31:11 +0530

    again

---
+++ src/advancedConcepts/generics/Box.java
@@ -0,0 +1,9 @@
+package advancedConcepts.generics;
+
+public class Box<T, E> {
+	T x;
+	T y;
+	T z;
+	E flag;
+	int a;
+}
---
+++ src/advancedConcepts/generics/Client.java
@@ -0,0 +1,15 @@
+package advancedConcepts.generics;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Box<Integer, Boolean> box = new Box<>();
+		box.x=10;
+		box.y=20;
+		box.z=30;
+		box.flag=true;
+		
+	}
+
+}
