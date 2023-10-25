a83d6164803471907017f01dd5dde8ecb6dfc2dc
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-10-26 00:04:12 +0530

    again

---
+++ src/assign121_11/A.java
@@ -0,0 +1,14 @@
+package assign121_11;
+public class A {
+    synchronized void fun1(){
+    	System.out.println("Fun1");
+    }
+
+    synchronized  void fun2(){
+    	System.out.println("Fun2");
+    }
+
+    void fun3(){
+    	System.out.println("Fun3");
+    }
+}
